32fb6a180b32ca2308e4c1e5a7a109ba74d82313
zhukovln09-dotcom <zhukovln09@gmail.com>
2026-01-21 19:35:11 +0300

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// Конфигурация Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyAevrUyamQxEa4QjT45CIJJKuBLeMaWIXE",
+  authDomain: "school-project-2026.firebaseapp.com",
+  projectId: "school-project-2026",
+  storageBucket: "school-project-2026.firebasestorage.app",
+  messagingSenderId: "10711525252",
+  appId: "1:10711525252:web:3cd555727ef10ac86edfd1",
+  measurementId: "G-KRBZ12Q0JW"
+};
